667946fef5ecc70c390729020aa38bc731638ed7
Ioan Vancea <ionut.vancea@gmail.com>
2015-07-29 00:53:24 +0200

    added animations and custom javascript code

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,40 @@
 $(document).ready(function() {
 
+    // wow.js initialization
+    new WOW().init();
 
+    // navbar animation feature
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 100) {
+            $('#navigation').removeClass('animated-header');
+        } else {
+            $('#navigation').addClass('animated-header');
+        }
+    });
+
+    // highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('.navbar-nav li a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+        $('.page-scroll a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
 
 });
 
